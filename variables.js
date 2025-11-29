076fb20b8fa5d53c7c267a8820b82e6584992176
souro-2003 <163489333+souro-2003@users.noreply.github.com>
2025-11-29 15:52:30 +0000

    commit done

---
+++ variables.js
@@ -0,0 +1,12 @@
+const accountId = 22356
+let name = "souro"
+let email="souro@gmail.com"
+var password = "@Hchuy5yk6a"
+var city  = "Bengaluru"
+let accountState
+
+// accountId=252515
+
+console.table([accountId,password,name,email,city,accountState])
+
+// prefer not to use var
